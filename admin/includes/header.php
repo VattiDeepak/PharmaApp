d432a4e4567cfd1a37bc079d553f69be4d920086
VattiDeepak <110245611+VattiDeepak@users.noreply.github.com>
2023-07-14 21:41:21 +0530

    Add files via upload

---
+++ admin/includes/header.php
@@ -0,0 +1,11 @@
+<header class="navbar navbar-dark  sticky-top bg-dark flex-md-nowrap  shadow ">
+    <a href="index.php" style="color:white;text-decoration:none;padding-left:10px;"><h3>PHARMAZONE</h3></a>
+
+    <div class="navbar-nav">
+        <div class="nav-item text-nowrap">
+            <a class="nav-link px-3" href="logout.php">Sign out</a>
+        </div>
+
+    </div>
+
+</header>
